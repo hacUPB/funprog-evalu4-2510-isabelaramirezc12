9ef61eaac9f1c77663fb75f08708e99dfb9d0cc2
Isabela Ramírez <isabela.ramirezc@upb.edu.co>
2025-05-15 09:24:49 -0500

    Se añadieron avances al repositorio

--- Reto/Reto.c
+++ Reto/Reto.c
@@ -18,14 +18,56 @@ printf("Por favor ingrese el nombre del archivo de entrada (.txt): ");
     }
 
     do {
-        printf("\n--- MENÚ DE ANÁLISIS ---\n");
+        printf("\nMENÚ\n");
         printf("1. Ver estadísticas del texto\n");
         printf("2. Ver frecuencia de vocales\n");
         printf("3. Buscar y reemplazar palabra\n");
         printf("4. Guardar resultados en archivo\n");
         printf("5. Salir\n");
-        printf("Seleccione una opción: ");
+        printf("Por favor seleccione una opción: ");
         scanf("%d", &opcion);
 
     }
+}
+
+do { ();
+switch (opcion){
+switch 
+
+case 1:
+
+break;
+
+case 2:
+
+break;
+
+case 3:
+
+break;
+
+case 4:
+
+break;
+
+case 5:
+    printf("4. Volver al menú principal\n");
+break;
+
+default:
+    printf("Errorr, esa opción no está disponible, seleccione otra nuevamente \n");
+break;
+
+}
+
+} while(parametro !=5); 
+
+return 0;
+
+
+// Funciones
+
+char*cargarArchivo(const char* nombreArchivo){
+
+
 }
